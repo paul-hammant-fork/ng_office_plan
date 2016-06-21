5c0aaad4209385255c4c993951a9ba2d55478e5f
Paul Hammant <paul@hammant.org>
2016-06-21 05:08:12 -0400

    progress

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-angular.module('project', ['ngResource'])
+angular.module('project', ['ngResource', 'base64'])
     .factory('ProjectMongo', function ($resource) {
         var ProjectMongo = $resource(':protocol//:server/data/:db/:q/:r',
             {protocol: 'http:', server: 'localhost:8080', db: 'seatmap', q: 'seats'},
@@ -8,16 +8,33 @@ angular.module('project', ['ngResource'])
         };
         return ProjectMongo;
     })
-    .controller('TheController', function ($scope, ProjectMongo) {
-        // TODO - this is as far as I got
-        ProjectMongo.get({ sort_by: '_id'}, function(rawseats) {
-            $scope.const = {
-                reduceOpacity: "fill-opacity:0.2"
-            };
-            $scope.seats = {};
-            angular.forEach(rawseats.rows, function (value, key) {
-                $scope.seats[value.doc.seat] = value.doc;
-            });
+    .controller('TheController', function ($scope, $base64, $http, $q, ProjectMongo) {
+
+        var credentials = $base64.encode("editor:changeit");
+        $http.defaults.headers.common["Authorization"] = 'Basic ' + credentials;
+        var deferred = $q.defer();
+        var request = $http.get('http://localhost:8080/_logic/roles/editor', {});
+        request.success(function (data, status, headers, config) {
+            if (!angular.isUndefined(data) && data !== null && !angular.isUndefined(data.authenticated) && data.authenticated) {
+                // TODO - gets this far then barfs with a 401 after a successful (silent) OPTIONS
+                ProjectMongo.get({ sort_by: '_id'}, function(rawseats) {
+                    $scope.const = {
+                        reduceOpacity: "fill-opacity:0.2"
+                    };
+                    $scope.seats = {};
+                    angular.forEach(rawseats.rows, function (value, key) {
+                        $scope.seats[value.doc.seat] = value.doc;
+                    });
+                });
+                deferred.resolve();
+
+            } else {
+                deferred.reject('authentication failed..');
+            }
+        });
+        request.error(function (data, status, header, config) {
+            console.log('authentication error');
+            deferred.reject('authentication failed..');
         });
 
         function getSeat(id) {
